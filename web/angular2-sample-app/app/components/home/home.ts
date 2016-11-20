3f0bef22561d73b059a96787c55063d7a252b208
Juan Carlos Hernández <Juan Carlos Hernndez>
2016-11-20 23:27:59 +0000

    First version of the homepage with map and logo

--- web/angular2-sample-app/app/components/home/home.ts
+++ web/angular2-sample-app/app/components/home/home.ts
@@ -1,8 +1,9 @@
 import {Component} from 'angular2/core';
-
+import {MapCmp} from '../map/map';
 @Component({
   selector: 'home',
   templateUrl: './components/home/home.html',
-  styleUrls: ['./components/home/home.css']
+  styleUrls: ['./components/home/home.css'],
+  directives: [ MapCmp ]
 })
 export class HomeCmp {}
