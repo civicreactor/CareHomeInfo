237599363370b6ec3785f0aa17df48f7a63324fd
Juan Carlos Hernández <Juan Carlos Hernndez>
2016-12-15 20:27:17 +0000

    app component moved to its folder

--- web/CivicReactor-Carehome-App/src/app/components/app.module.ts
+++ web/CivicReactor-Carehome-App/src/app/components/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { AboutCmp } from './about/about';
 import { HomeCmp } from './home/home';
 import { MapCmp } from './map/map';
