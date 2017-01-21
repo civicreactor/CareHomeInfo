f5ee622dd433509d306626ced589138db26e32fd
Juan Carlos Hernández <Juan Carlos Hernndez>
2017-01-21 20:24:27 +0000

    routes fix

--- express/index.js
+++ express/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var staticRoot = __dirname + '/public/';
 
-app.use(express.static('public'));
+app.use(express.static(staticRoot));
 
 var routes = require('./apiRoutes.js')
 app.use('/api', routes);
+
+app.use(function(req, res, next){
+  fs.createReadStream(staticRoot + 'index.html').pipe(res);
+});
 
 app.listen(port, function () {
   console.log('App listening on port', port)
